09ad987ef3842aff684edd90c36bd0038fe80df9
amitchakradhari <amitchakradhari2406@gmail.com>
2021-04-01 17:19:57 +0530

    first commit

---
+++ MudBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MudBot
+//
+//  Created by Amit  Chakradhari on 01/04/21.
+//  Copyright © 2021 Amit  Chakradhari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
